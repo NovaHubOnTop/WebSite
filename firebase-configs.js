857c43a06a4462eb65f89460630ae7cb3beae3b2
Encrypted <NovaHubMuscleLegends@gmail.com>
2025-02-19 20:05:19 +0100

    Update firebase-configs.js

--- firebase-configs.js
+++ firebase-configs.js
@@ -1,22 +1,22 @@
 // firebase-configs.js
 
-// Import the necessary Firebase functions
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-analytics.js";
+// Import the necessary Firebase SDKs
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration (replace these with your Firebase details)
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDAh-m6Jj5kuP72rPMGX11W5akOjH6A0h4",
   authDomain: "mywebsite-51f44.firebaseapp.com",
   projectId: "mywebsite-51f44",
-  storageBucket: "mywebsite-51f44.firebasestorage.app",
+  storageBucket: "mywebsite-51f44.appspot.com",
   messagingSenderId: "470416155004",
   appId: "1:470416155004:web:de6e35796b6bddf57704f9",
   measurementId: "G-2CE3LSKWB1"
 };
 
-// Initialize Firebase with your config
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Analytics (optional, if you plan to use analytics)
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
